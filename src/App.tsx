d4a77702ae5d9b76ca6e02d7241ce7f3c299cdc0
diego <diego@debian.debian>
2025-08-21 12:15:54 -0500

    use-efect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import { useState } from "react";
-import { Button } from "./components";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
-const buttonStyles = {
-  primary: {
-    color: "#ffffff",
-    background: "#651fff",
-    transition: "all 1s ease",
-  },
-  secondary: {
-    color: "#651fff",
-    background: "#ffffff",
-    transition: "all 1s ease",
-  },
-  dark: { color: "#ffffff", background: "#5d5d5d", transition: "all 1s ease" },
-};
-function App() {
-  const [data, setData] = useState(0);
+const URL = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
-  return <></>;
+function App() {
+  const [data, setData] = useState<{ name: string }[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  const consoleLoader = (loadingValue: boolean) => {
+    setLoading(loadingValue);
+    console.info(loadingValue);
+  };
+
+  const fechtData = async () => {
+    try {
+      consoleLoader(true);
+      const response = await fetch(URL);
+      if (!response.ok) throw new Error("Error fetching data");
+      const jsonData = await response.json();
+      setData(jsonData.results);
+    } catch (error) {
+      setError(error as string);
+      console.log("Error fetching data: ", error);
+    } finally {
+      consoleLoader(false);
+    }
+  };
+
+  // comincarnos con un endpoint
+  // operacion asincronas
+  // parametros de entrada
+  // context
+  useEffect(() => {
+    fechtData();
+  }, []);
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>Upps! sucedio un error: {error}</p>;
+  return (
+    <>
+      <ul>
+        {data.map((item) => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
